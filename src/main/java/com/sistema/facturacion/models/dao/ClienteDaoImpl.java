69cd0f054bea91adb39e88d349486fc24ed9cc14
Gino Ascencio <gino.francisco@hotmail.com>
2021-08-06 00:52:04 -0500

    CRU clientes

---
+++ src/main/java/com/sistema/facturacion/models/dao/ClienteDaoImpl.java
@@ -0,0 +1,38 @@
+package com.sistema.facturacion.models.dao;
+
+import com.sistema.facturacion.models.entity.Cliente;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class ClienteDaoImpl implements IClienteDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Cliente> findAll() {
+        return em.createQuery("from Cliente").getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void save(Cliente cliente) {
+        if (cliente.getId() != null && cliente.getId() > 0) {
+            em.merge(cliente);
+        } else {
+            em.persist(cliente);
+        }
+    }
+
+    @Transactional
+    @Override
+    public Cliente findOne(Long id) {
+        return em.find(Cliente.class, id);
+    }
+}
---
+++ src/test/java/com/sistema/facturacion/FacturacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.facturacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
