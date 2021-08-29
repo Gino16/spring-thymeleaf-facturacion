697514c8072a4cdf1d2cc85889e62a3516ba2905
Gino16 <gino.francisco@hotmail.com>
2021-08-28 22:57:39 -0500

    Para deploy, ver el tema de almacenamiento en servicio de tercero

--- src/main/java/com/sistema/facturacion/FacturacionApplication.java
+++ src/main/java/com/sistema/facturacion/FacturacionApplication.java
@@ -1,12 +1,17 @@
 package com.sistema.facturacion;
 
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FacturacionApplication {
+    private static final String ACCESS_TOKEN = "sl.A3coi6dYLk2bEQekIiSw9IiwdUAZboQa62TyLfl3Wiv1qNyXZlkksjjCGYcgiOYZAd6Rfoc47R-Jv_0NSMb9WsIkW_8YzuiRVTw1iPO1JiJDj3nKhBbDOx1cKQNz8umBkdZshyw";
 
     public static void main(String[] args) {
+        DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/prueba-factura-spring").build();
+        DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
         SpringApplication.run(FacturacionApplication.class, args);
     }
 
--- src/main/java/com/sistema/facturacion/SpringSecurityConfig.java
+++ src/main/java/com/sistema/facturacion/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.sistema.facturacion;
 
+import com.sistema.facturacion.auth.handler.LoginSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private LoginSuccessHandler successHandler;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -29,7 +33,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/factura/**").hasAnyRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/login").permitAll()
+                .formLogin().successHandler(successHandler).loginPage("/login").permitAll()
                 .and()
                 .logout().permitAll()
                 .and()
